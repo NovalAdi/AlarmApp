22a515571c611e862318586e12f8c829907e2f5d
NovalAdi <novaladiperasetya@gmail.com>
2022-01-17 15:08:17 +0700

    Initial commit

---
+++ app/src/main/java/com/vall/myapplication/OneTImeAlarmActivity.kt
@@ -0,0 +1,11 @@
+package com.vall.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OneTImeAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one_time_alarm)
+    }
+}
